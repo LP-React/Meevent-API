c398cacb6cff29537ebd8e15baedfeb328d1fbc3
Gabriel <i202332157@cibertec.edu.pe>
2025-12-19 01:37:44 -0500

    feat: crear proyecto nuevo para grcp

---
+++ gRcp_Meevent/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using gRcp_Meevent;
+using Grpc.Core;
+
+namespace gRcp_Meevent.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
